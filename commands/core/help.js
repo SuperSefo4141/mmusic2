12c13903932915ef14c300ccb053acb565db1398
SuperSefo4141 <140642587+SuperSefo4141@users.noreply.github.com>
2023-12-03 20:12:24 +0300

    sa

---
+++ commands/core/help.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'help',
+    aliases: ['h',"yardım","y"],
+    showHelp: false,
+    utilisation: '{prefix}help',
+
+    execute(client, message, args) {
+        const embed = new MessageEmbed();
+
+        embed.setColor(client.config.messageColor);
+        embed.setAuthor(client.user.username, client.user.displayAvatarURL({ size: 1024, dynamic: true }));
+
+        const commands = client.commands.filter(x => x.showHelp !== false);
+
+        embed.setDescription('Sende kendi özel müzik botunu oluşturmak istiyorsan Youtubeden AOPRAX\'ı takip et!');
+        embed.addField(`Kullanılabilir - ${commands.size} Komut Var`, commands.map(x => `\`${x.name}${x.aliases[0] ? ` (${x.aliases.map(y => y).join(', ')})\`` : '\`'}`).join(' | '));
+
+        embed.setTimestamp();
+        embed.setFooter('Müzik Botu Komutları ~AOPRAX', message.author.avatarURL({ dynamic: true }));
+
+        message.channel.send({ embeds: [embed] });
+    },
+};
